83c242cf4caebd0d40d38febb720e3d9c382651d
Alex Abalo <alex.abalodm@gmail.com>
2026-06-25 18:37:27 -0300

    Agrego extracción de datos PDF

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const pdfFile = document.getElementById("pdfFile");
 const btnProcesar = document.getElementById("btnProcesar");
 const resultado = document.getElementById("resultado");
 
+function extraerDato(texto, regex) {
+    //texto match, busca patron del texto
+    const resultado = texto.match(regex);
+
+    return resultado
+        ? resultado[1].trim()
+        : "No encontrado";
+}
+
 btnProcesar.addEventListener("click",async ()=> {
     const archivo = pdfFile.files[0];
 
@@ -24,16 +33,90 @@ btnProcesar.addEventListener("click",async ()=> {
         // recorre con map todos los str y los devuelve a texto normal
         const texto = contenido.items.map(item => item.str).join(" ");
 
-         console.log("Pagina:", paginaNumber);
+        console.log(texto.substring(0, 1000));
 
+        console.log(texto.includes("Fecha"));
+        console.log(texto.match(/\d{2}-\d{2}-\d{2}/))
+
+         console.log("Pagina:", pagina.pageNumber);
+         console.log(texto);
+
+         console.log(
+            texto.indexOf("Fecha")
+         );
+
+         console.log(
+            texto.substring(
+                texto.indexOf("Fecha"),
+                texto.indexOf("Fecha") + 150
+            )
+         );
+
+         console.log(
+            texto.substring(
+                texto.indexOf("Dominio"),
+                texto.indexOf("Dominio") + 100
+            )
+         );
+
+         console.log(
+            texto.substring(
+                texto.indexOf("Modelo"),
+                texto.indexOf("Modelo") + 100
+            )
+         );
+
+         const fecha = extraerDato(
+            texto,
+            /Fecha.*?(\d{2}-\d{2}-\d{2})/
+         );
+
+         const marca = extraerDato(
+            texto, 
+            /marca:\s*([A-Z]+)/i
+         );
+
+          const interno =
+            extraerDato(
+                texto,
+                /Interno:\s*(\d+)/
+            );
+
+            //esto busca el dominio
+             const dominioMatch = texto.match(
+                  /Dominio:\s*([A-Z]{3}\s*\d{3})/i
+             );
+
+             //este lo guardo en el caso de coincidir
+          const dominio = dominioMatch
+          ? dominioMatch[1].trim()
+          : "No encontrado";
+
+
+
+        const modeloMatch =
+texto.match(
+    /Modelo:\s*([A-Z0-9 ]+?)\s+Marca Caja:/i
+);
+
+const modelo =
+modeloMatch
+    ? modeloMatch[1].trim()
+    : "No encontrado";
 
                 //remplazo temporalmente
         //resultado.innerHTML = `PDF cargado.
         //Total de paginas: ${pdf.numPages}`;
 
-        resultado.innerHTML = `<h3>Pagina 1</h3>
+        resultado.innerHTML = `<div class="text-left">
+        <p><strong>Fecha:</strong> ${fecha}</p>
+        <p><strong>Marca:</strong> ${marca}</p>
+        <p><strong>Interno:</strong> ${interno}</p>
+        <p><strong>Dominio:</strong> ${dominio}</p>
+        <p><strong>Modelo:</strong> ${modelo}</p>
+        </div>
 
-        <pre>${texto}</pre>`;
+        <pre class="mt-6 p-4 bg-slate-100 rounded-lg overflow-auto text-sm">${texto}</pre>`;
 
 
     }
