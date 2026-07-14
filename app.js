bf8f190ffd215323fb3cae0796a2fbbe4d9e49bc
Alex Abalo <alex.abalodm@gmail.com>
2026-07-13 21:41:11 -0300

    cambio

--- app.js
+++ app.js
@@ -35,26 +35,96 @@ async function leerVehiculo(pagina) {
         /Interno:\s*(\d+)/
     );
 
-    
+    const dominio = extraerDato(
+        texto,
+        /Dominio:\s*([A-Z]{3}\s*\d{3})/i
+    );
 
+    const modelo = extraerDato(
+        texto,
+         /Modelo:\s*([A-Z0-9 ]+?)\s+Marca Caja:/i
+    );
+
+    const anio = extraerDato(
+        texto,
+        /Año:\s*(\d{4})/
+    );
+
+    const humo = extraerDato(
+        texto,
+        /Densidad del Humo.*?(\d+,\d+)/
+    );
+
+    const resultadoHumo = extraerDato(
+        texto,
+        /Densidad del Humo[\s\S]*?Resultado\s+(\w+)/
+
+
+    )
 
     const vehiculo = {
         fecha,
         marca,
         interno,
-
+        dominio,
+        modelo,
+        anio,
+        humo,
+        resultadoHumo
     };
 
     return vehiculo;
 
-
 }
 
-//dominio,
-// modelo,
-// anio,
-// humo,
-// resultadoHumo
+function mostrarTabla(vehiculos) {
+    let html = `
+    <h2>Vehiculos encontrados: ${vehiculos.length}</h2>
+    
+    <table border="1" cellpadding="5">
+        <thead>
+             <tr>
+                    <th>Fecha</th>
+                    <th>Marca</th>
+                    <th>Interno</th>
+                    <th>Dominio</th>
+                    <th>Modelo</th>
+                    <th>Año</th>
+                    <th>Humo</th>
+                    <th>Resultado</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+    vehiculos.forEach(v => {
+
+           html += `
+            <tr>
+                <td>${v.fecha}</td>
+                <td>${v.marca}</td>
+                <td>${v.interno}</td>
+                <td>${v.dominio}</td>
+                <td>${v.modelo}</td>
+                <td>${v.anio}</td>
+                <td>${v.humo}</td>
+                <td>${v.resultadoHumo}</td>
+            </tr>
+        `;
+
+    });
+
+
+     html += `
+            </tbody>
+        </table>
+    `;
+
+    resultado.innerHTML = html;
+}
+
+
+
 
 btnProcesar.addEventListener("click", async () => {
     const archivo = pdfFile.files[0];
@@ -63,6 +133,8 @@ btnProcesar.addEventListener("click", async () => {
         resultado.textContent = "Selecciona un archivo PDF";
         return;
     }
+
+  
 
     try {
         const arrayBuffer = await archivo.arrayBuffer();
@@ -79,23 +151,15 @@ btnProcesar.addEventListener("click", async () => {
             vehiculos.push(vehiculo);
         }
 
-        console.table(vehiculos);
-
-        resultado.innerHTML = `
-        <h2 class="text-xl font-bold mb-4">
-        Vehiculos encontrados: ${vehiculos.length}
-        </h2>
-        <pre>${JSON.stringify(vehiculos, null, 2)}</pre>
-        `;
-
-
+             console.table(vehiculos);
+             mostrarTabla(vehiculos);
 
     }
+
+ 
 
     catch (error) {
         console.error(error);
         resultado.textContent = "Error al leer PDF";
     }
-
-
 });
